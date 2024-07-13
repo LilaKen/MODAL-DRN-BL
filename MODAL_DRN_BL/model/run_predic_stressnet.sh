1f33d78920e85f9ec2adc9d8705afc87f26dc4e5
Lil_Ken <98253413+LilaKen@users.noreply.github.com>
2024-07-13 15:42:38 +0800

    Delete MODAL_DRN_BL directory

--- MODAL_DRN_BL/model/run_predic_stressnet.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-nohup python -u predic_stressnet.py train  -g 1 > ../log/stressnet_pre.log 2>&1 &
-nohup python -u predic_scsnet.py train  -g 1 > ../log/scsnet_pre.log 2>&1 &
-nohup python -u predic_inbetween.py train  -g 1 > ../log/inbetween_pre.log 2>&1 &
-
--- MODAL_DRN_BL/model/test_predic_nogan_resnet.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-nohup python -u predic_nogan.py test -a res_c_26 --resume res_c_26_model_nogan_resnet_best.pth.tar -g 1 > nogantestres26pre.log 2>&1 &
-nohup python -u predic_nogan.py test -a res_c_42 --resume res_c_42_model_nogan_resnet_best.pth.tar -g 1 > nogantestres42pre.log 2>&1 &
-nohup python -u predic_nogan.py test -a res_d_22  --resume res_d_22_model_nogan_resnet_best.pth.tar -g 2 > nogantestres22pre.log 2>&1 &
-nohup python -u predic_nogan.py test -a res_d_38  --resume res_d_38_model_nogan_resnet_best.pth.tar -g 2 > nogantestres38pre.log 2>&1 &
-
-
-#后期修改resume 添加nogan
